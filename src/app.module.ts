4ff655d6ac7528462ee783c6573b6d9266033eb3
Daniel <daniilterawars@gmail.com>
2022-06-05 16:30:16 +0300

    feat: init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { databaseProviders } from './database.providers';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [...databaseProviders, AppService],
+  exports: [...databaseProviders],
 })
 export class AppModule {}
