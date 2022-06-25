cc2f28b5925e41767c9cdbee87fa4b3a0b4c2652
Daniel <daniilterawars@gmail.com>
2022-06-25 23:57:48 +0300

    feat: ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common';
 import { databaseProviders } from './database.providers';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController, AuthController],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required,
+        POSTGRES_PORT: Joi.number().required,
+        POSTGRES_USER: Joi.string().required,
+        POSTGRES_PASSWORD: Joi.string().required,
+        POSTGRES_DB: Joi.string().required,
+        PORT: Joi.number(),
+      }),
+    }),
+  ],
+  controllers: [AppController],
   providers: [...databaseProviders, AppService],
   exports: [...databaseProviders],
 })
